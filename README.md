e192db2ee4936e50948485647f00d38820f19ea0
Pijush Gupta <Pijush@live.com>
2022-12-29 15:12:16 +0530

    readme update

---
+++ README.md
@@ -0,0 +1,28 @@
+#### Php Router
+
+###### Clone this repository
+
+```bash
+git clone https://github.com/Pijushgupta/php-router
+
+```
+
+###### Adding it to the project
+
+```php
+include_once 'Location of file';
+```
+
+OR use Composer/auto-loader
+
+###### Using it to the project
+
+```php
+use phpRouter\Router;
+```
+
+```php
+new Router('/', function () {
+	echo 'Home';
+});
+```
---
+++ router.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace phpRouter;
+
+class Router {
+	/**
+	 * $router - to store the routes and callback
+	 */
+	private $routes = [];
+
+	/**
+	 * $serverUri - to store the current url
+	 */
+	private $serverUri;
+
+
+
+	/**
+	 * to add routes 
+	 */
+	public function __construct(string $path, callable $callback) {
+
+		if ($path && $callback) {
+			$this->routes[$path] = $callback;
+		}
+	}
+
+	/**
+	 * to run methods associated with routes
+	 */
+	private function run() {
+		$this->serverUri = $_SERVER['REQUEST_URI'];
+
+
+		foreach ($this->routes as $path => $callback) {
+			if ($this->serverUri === $path) $callback();
+		}
+	}
+
+
+
+	/**
+	 * run the run method after object creation in done, so user don't have to call run method manually 
+	 */
+	public function __destruct() {
+		if (empty($this->routes) || $this->routes == NULL) return;
+		$this->run();
+	}
+}
